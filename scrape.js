e88fd8e50254bacc354a066e5f934998f8667971
Stefan Graupner <stefan.graupner@gmail.com>
2014-03-27 08:35:55 +0100

    scraping works for boroughs.

--- scrape.js
+++ scrape.js
@@ -16,61 +16,48 @@ ba_list = [
 ];
 
 var beschluesse = new Array;
-var current = 0;
+var current = 5;
 
 function buildURL(ba)
 {
-  return "http://www.berlin.de/" + ba + "/bvv-online/vo040.asp";
+  return "http://www.berlin.de/" + ba + "/bvv-online/vo040.asp"; //"http://www.berlin.de/" + ba + "/bvv-online/vo040.asp";
 }
 
 function getDocs()
 {
   var rows = document.querySelectorAll('#rismain_raw tbody tr');
-  var ret = new Array;
+  var result = new Array;
 
   for (i = 0; i < rows.length; i++)
   {
-    link = rows[i].getElementsByTagName('a');
-    d    = rows[i].getElementsByTagName('td')[4];
+    var link = rows[i].getElementsByTagName('a');
+    var date_col = rows[i].getElementsByTagName('td');
+    
+    date = date_col[4];
+    if (date) date = date.innerText;
 
-    dcheck = d[0].innerText.split('.');
-    dcheck = new Date(Date.UTC(parseInt(dcheck[2]), parseInt(dcheck[1]), parseInt(dcheck[0])));
-
-    today = new Date();
-
-    if (link 
-    &&  link.length == 1 
-    &&  !link[0].href.endsWith('?showall=true'))
-    {
-      ret.push({
-        "description": link[0].innerText,
-        "href": link[0].href,
-        "date": d[0].innerText
-      });
-    }
+    result.push({ "link": link[0].href, "description": link[0].innerText, "date": date });
   }
 
-  return ret;
+  return result;
 }
 
-/*for (i = 0; i < ba_list.length; i++)
-{
-  
-}*/
+function type(obj){
+    return Object.prototype.toString.call(obj).slice(8, -1);
+}
 
 var casper = require('casper').create({
   pageSettings: 
   {
-    loadImages: false,
-    loadPlugins: false
+    loadImages: false
+    //loadPlugins: false
   }
 });
 
 var fs = require('fs');
+var url = buildURL(ba_list[current]);
 
-casper.start();
-
-casper.open(buildURL(ba_list[current]), 
+casper.start().open(url, 
 {
   'method': 'post',
   'data': 
@@ -82,25 +69,23 @@ casper.open(buildURL(ba_list[current]),
     x: 9,
     y: 12
   }
-});
-
-casper.then(function()
+}).then(function()
 {
   beschluesse = beschluesse.concat(this.evaluate(getDocs));
-  console.log(beschluesse.length);
-})
-
-casper.run(function() {
-  // this.echo(JSON.stringify(beschluesse));
+}).run(function() {
   var out = "";
-  
+
   for (i = 0; i < beschluesse.length; i++)
   {
-    out += "beschluesse[i].date" + ": " + beschluesse[i].href + "\n\n" + beschluesse[i].description + "\n\n---\n\n";
+    //dcheck = beschluesse[i].date.split('.');
+    //dcheck = new Date(Date.UTC(parseInt(dcheck[2]), parseInt(dcheck[1]), parseInt(dcheck[0])));
+
+    //console.log(dcheck);
+
+    out += beschluesse[i].date + ": " + beschluesse[i].link + "\n\n" + beschluesse[i].description + "\n\n---\n\n";
   }
 
-  //fs.write("beschluesse.txt", out, 'w');
-  this.echo(out);
+  fs.write("beschluesse.txt", out, 'w');
 
   this.exit();
 });

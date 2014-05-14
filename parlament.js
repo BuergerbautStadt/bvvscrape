d201e5bfe1a12b2bc750164697743dc2ef232f62
Stefan Graupner <stefan.graupner@gmail.com>
2014-05-14 20:57:18 +0200

    Dates...

--- parlament.js
+++ parlament.js
@@ -26,6 +26,7 @@ casper.thenEvaluate(function() {
   sel = document.querySelector('#Beschreibung');
   sel.value = 'Bebauungsplan';
 
+  // this website, it's crazy
   applyFilter();
 });
 
@@ -40,22 +41,25 @@ casper.then(function() {
     {
       var description = rows[i].querySelector('td > strong').innerText;
       var date  = rows[i].childNodes[4].textContent.match(/(e|ü):([0-9.]+) /i);
-      var links = rows[i].querySelectorAll('a');
+      
+      var documents = new Array();
+
+      $('#vorgaenge tbody tr:nth-child('+i+') a').each(function() { documents.push(this.href) });
+
+      
 
       if (date)
       {
+        d = date[2].split('.');
+        d = new Date(parseInt(d[2]), parseInt(d[1]), parseInt(d[0]));
+        
+        xdate = new XDate(d);
+
         result.push({
           "description": description,
-          "links": new Array()
-        })
-
-        /*for (j = 0; j < links.length; j++)
-        {
-          result["links"].push({
-            "href": links[j].href,
-            "description": links[j].innerText
-          });
-        }*/
+          "documents": documents,
+          "date": xdate.toISOString()
+        });
       }
     }
 
